f1b7338920d1ce556e9ca153000eb1cc6ea0b2d5
Olivier Cornet <o.cornet2@gmail.com>
2019-10-12 11:50:32 +0200

    Add basic player movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	public float sensitivityX = 15F;
-	public float sensitivityY = 15F;
+	public float cameraSensitivityX = 100f;
+	public float cameraSensitivityY = 100f;
 
-	public float minimumY = -60F;
-	public float maximumY = 60F;
+	public float MoveSpeed = 10f;
 
 	private float rotationY = 0F;
 
 	private Camera _camera;
+	private CharacterController _controller;
 
 	private void Start()
 	{
 		_camera = GetComponentInChildren<Camera>();
+		_controller = GetComponent<CharacterController>();
 	}
 
 	void Update()
 	{
+		UpdateCameraRotation();
+		UpdateMovement();
+	}
+
+	private void UpdateCameraRotation()
+	{
 		Cursor.lockState = CursorLockMode.Locked;
-		float rotationX = _camera.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX * Time.deltaTime;
+		float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * cameraSensitivityX * Time.deltaTime;
 
-		rotationY += Input.GetAxis("Mouse Y") * sensitivityY * Time.deltaTime;
-		rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+		rotationY += Input.GetAxis("Mouse Y") * cameraSensitivityY * Time.deltaTime;
+		rotationY = Mathf.Clamp(rotationY, -70f, +70f);
 
-		_camera.transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		transform.localEulerAngles = new Vector3(0, rotationX, 0);
+		_camera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+	}
+
+	private void UpdateMovement()
+	{
+		float forwardInput = Input.GetAxis("Vertical");
+		float lateralInput = Input.GetAxis("Horizontal");
+
+		Vector3 movement = transform.forward * forwardInput + transform.right * lateralInput;
+		// cap the max speed so that the player doesn't go faster diagonally
+		if (movement.sqrMagnitude > 1f) movement.Normalize(); 
+
+		_controller.Move(movement * MoveSpeed * Time.deltaTime);
 	}
 }
