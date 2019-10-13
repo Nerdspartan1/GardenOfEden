9a949b362977ebf5d9dd17ca7f6cb5d7596eef50
Olivier Cornet <o.cornet2@gmail.com>
2019-10-13 18:21:52 +0200

    Add negative shader

---
+++ Assets/Scripts/CameraShader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraShader : MonoBehaviour
+{
+	public Shader Shader;
+	private Material _material;
+
+	void Start()
+	{
+		_material = new Material(Shader);
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination, _material);
+	}
+}
